7956b9b91e4ae61df86e3bb27e0405106f020c8c
Piotr Kliczewski <piotr.kliczewski@gmail.com>
2018-02-06 12:22:24 +0100

    Merge pull request #203 from borod108/add_specs_for_targeted
    Add fix and add specs for graph targeted refresh
    (cherry picked from commit 744559cf973bc42b71fcaabae35759e314561797)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1510499

--- app/models/manageiq/providers/redhat/inventory_collection_default/infra_manager.rb
+++ app/models/manageiq/providers/redhat/inventory_collection_default/infra_manager.rb
@@ -242,6 +242,61 @@ class ManageIQ::Providers::Redhat::InventoryCollectionDefault::InfraManager < Ma
       attributes.merge!(extra_attributes)
     end
 
+    def hosts(extra_attributes = {})
+      attributes = {
+        :model_class                 => ::Host,
+        :association                 => :hosts,
+        :manager_ref                 => [:uid_ems],
+        :inventory_object_attributes => %i(
+          type
+          ems_ref
+          ems_ref_obj
+          name
+          hostname
+          ipaddress
+          uid_ems
+          vmm_vendor
+          vmm_product
+          vmm_version
+          vmm_buildnumber
+          connection_state
+          power_state
+          ems_cluster
+          ipmi_address
+          maintenance
+        ),
+        :builder_params              => {
+          :ems_id => ->(persister) { persister.manager.id },
+        },
+        :custom_reconnect_block      => lambda do |inventory_collection, inventory_objects_index, attributes_index|
+          relation = inventory_collection.model_class.where(:ems_id => nil)
+
+          return if relation.count <= 0
+
+          inventory_objects_index.each_slice(100) do |batch|
+            relation.where(inventory_collection.manager_ref.first => batch.map(&:first)).each do |record|
+              index = inventory_collection.object_index_with_keys(inventory_collection.manager_ref_to_cols, record)
+
+              # We need to delete the record from the inventory_objects_index and attributes_index, otherwise it
+              # would be sent for create.
+              inventory_object = inventory_objects_index.delete(index)
+              hash             = attributes_index.delete(index)
+
+              record.assign_attributes(hash.except(:id, :type))
+              if !inventory_collection.check_changed? || record.changed?
+                record.save!
+                inventory_collection.store_updated_records(record)
+              end
+
+              inventory_object.id = record.id
+            end
+          end
+        end
+      }
+
+      attributes.merge!(extra_attributes)
+    end
+
     def root_folders(extra_attributes = {})
       attributes = {
         :model_class                 => ::EmsFolder,
