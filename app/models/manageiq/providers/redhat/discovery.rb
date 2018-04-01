f26df63e4c6676bfdc967e80b1be4a0a82435d98
Moti Asayag <masayag@redhat.com>
2018-04-01 10:56:49 +0300

    Merge pull request #232 from borod108/bugs/discovery1559796
    Fix provider discovery
    (cherry picked from commit 9e95b90ada4f822ae971fc2565bd46d1f0e82255)
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1562788

--- app/models/manageiq/providers/redhat/discovery.rb
+++ app/models/manageiq/providers/redhat/discovery.rb
@@ -6,10 +6,10 @@ module ManageIQ
     module Redhat
       class Discovery
         def self.probe(ost)
-          Ovirt.logger = $rhevm_log if $rhevm_log
+          ::Ovirt.logger = $rhevm_log if $rhevm_log
 
-          if ManageIQ::NetworkDiscovery::Port.open?(ost, Ovirt::Service::DEFAULT_PORT) &&
-             Ovirt::Service.ovirt?(:server => ost.ipaddr, :verify_ssl => false)
+          if ManageIQ::NetworkDiscovery::Port.open?(ost, ::Ovirt::Service::DEFAULT_PORT) &&
+             ::Ovirt::Service.ovirt?(:server => ost.ipaddr, :verify_ssl => false)
             ost.hypervisor << :rhevm
           end
         end
