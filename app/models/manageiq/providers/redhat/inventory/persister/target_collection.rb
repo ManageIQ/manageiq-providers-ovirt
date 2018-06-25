ee8be1298123aa5ab9cbabf75ffd6384eb5cfd8f
Martin Slemr <mslemr@redhat.com>
2018-06-25 14:23:16 +0200

    Join of :vm_and_template_ems_custom_fields ICs
    Definition of in targeted collection's persister on wrong place

--- app/models/manageiq/providers/redhat/inventory/persister/target_collection.rb
+++ app/models/manageiq/providers/redhat/inventory/persister/target_collection.rb
@@ -5,14 +5,6 @@ class ManageIQ::Providers::Redhat::Inventory::Persister::TargetCollection < Mana
     initialize_infra_inventory_collections
 
     @collection_group = nil
-
-    add_collection(infra, :vm_and_template_ems_custom_fields) do |builder|
-      builder.add_properties(
-        :model_class => ::CustomAttribute,
-        :manager_ref => %i(name),
-      )
-      builder.add_inventory_attributes(%i(section name value source resource))
-    end
   end
 
   # not added to IC properties
