1376845d3cf4f7996cd2e568123c2742a34659ef
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-20 15:44:08 +0200

    Add plugin display name

--- lib/manageiq/providers/ovirt/engine.rb
+++ lib/manageiq/providers/ovirt/engine.rb
@@ -3,6 +3,10 @@ module ManageIQ
     module Ovirt
       class Engine < ::Rails::Engine
         isolate_namespace ManageIQ::Providers::Ovirt
+
+        def self.plugin_name
+          _('oVirt Provider')
+        end
       end
     end
   end
