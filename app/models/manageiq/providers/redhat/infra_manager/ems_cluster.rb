ff87e8f8d9d939535332c60f963ec30ab08bdba6
Boris Odnopozov <bodnopoz@redhat.com>
2018-11-03 20:03:12 +0200

    Add support for CA in Ansible calls
    Support creating a file from the certificate attached to the provider
    and pass its path to the Ansible role.
    
    This is part of implementing: https://bugzilla.redhat.com/show_bug.cgi?id=1644605

--- app/models/manageiq/providers/redhat/infra_manager/ems_cluster.rb
+++ app/models/manageiq/providers/redhat/infra_manager/ems_cluster.rb
@@ -1,12 +1,18 @@
 class ManageIQ::Providers::Redhat::InfraManager::EmsCluster < ManageIQ::Providers::InfraManager::EmsCluster
   def upgrade(options = {})
     role_options = {:role_name => "oVirt.cluster-upgrade"}
-    job = ManageIQ::Providers::AnsibleRoleWorkflow.create_job({}, extra_vars_for_upgrade(options), role_options)
+    job = ManageIQ::Providers::AnsibleRoleWorkflow.create_job({}, extra_vars_for_upgrade(options), role_options, :hook_object => cert_file_creator)
     job.signal(:start)
     job.miq_task
   end
 
   private
+
+  def cert_file_creator
+    cert = ext_management_system.default_endpoint.certificate_authority
+    return nil unless cert
+    CertificateFileCreator.new(cert)
+  end
 
   def extra_vars_for_upgrade(options = {})
     connect_options = ext_management_system.apply_connection_options_defaults(options)
@@ -25,5 +31,18 @@ class ManageIQ::Providers::Redhat::InfraManager::EmsCluster < ManageIQ::Provider
       :cluster_name    => name,
       :hostname        => "localhost"
     }
+  end
+
+  class CertificateFileCreator
+    attr_accessor :cert
+    def initialize(cert)
+      @cert = cert
+    end
+
+    def after_dir_create(base_dir, _env_vars, extra_vars, _ansible_runner_method, _playbook_or_role_args)
+      local_filename = File.join(base_dir, "ca_#{SecureRandom.hex}")
+      File.open(local_filename, 'w') {|f| f.write(cert) }
+      extra_vars[:engine_cafile] = local_filename
+    end
   end
 end
--- spec/models/manageiq/providers/redhat/infra_manager/ems_cluster_spec.rb
+++ spec/models/manageiq/providers/redhat/infra_manager/ems_cluster_spec.rb
@@ -5,6 +5,7 @@ describe ManageIQ::Providers::Redhat::InfraManager::EmsCluster do
       @cluster = FactoryGirl.create(:ems_cluster_redhat, :ems_id => @ems.id)
       my_server = double("my_server", :guid => "guid1")
       allow(MiqServer).to receive(:my_server).and_return(my_server)
+      @ems.default_endpoint.update_attribute(:certificate_authority, "cert auth")
     end
     it "sends the right parameters to the upgrade" do
       env_vars = {}
@@ -14,7 +15,8 @@ describe ManageIQ::Providers::Redhat::InfraManager::EmsCluster do
                     :cluster_name    => @cluster.name,
                     :hostname        => "localhost"}
       role_arg = {:role_name=>"oVirt.cluster-upgrade"}
-      expect(ManageIQ::Providers::AnsibleRoleWorkflow).to receive(:create_job).with(env_vars, extra_args, role_arg).and_call_original
+      file_creator_class = ManageIQ::Providers::Redhat::InfraManager::EmsCluster::CertificateFileCreator
+      expect(ManageIQ::Providers::AnsibleRoleWorkflow).to receive(:create_job).with(env_vars, extra_args, role_arg, :hook_object => kind_of(file_creator_class)).and_call_original
       @cluster.upgrade
     end
   end
