19d892c460c1a8ea52715408c18154c3f7944421
Daniel Berger <dberger@redhat.com>
2019-02-04 15:47:26 -0500

    Remove the minimum version constant.

--- app/models/manageiq/providers/redhat/infra_manager/host.rb
+++ app/models/manageiq/providers/redhat/infra_manager/host.rb
@@ -1,6 +1,4 @@
 class ManageIQ::Providers::Redhat::InfraManager::Host < ::Host
-  MINIMUM_CONVERSION_HOST_VERSION = '4.2.4'.freeze
-
   def provider_object(connection = nil)
     ovirt_services_class = ManageIQ::Providers::Redhat::InfraManager::OvirtServices::Builder
                            .build_from_ems_or_connection(:ems => ext_management_system, :connection => connection)
@@ -27,9 +25,10 @@ class ManageIQ::Providers::Redhat::InfraManager::Host < ::Host
     end
   end
 
+  # The minimum supported version is 4.2.4, so we hard code it here.
   supports :conversion_host do
     version = ext_management_system.api_version
-    if version.nil? || Gem::Version.new(version) < Gem::Version.new(MINIMUM_CONVERSION_HOST_VERSION)
+    if version.nil? || Gem::Version.new(version) < Gem::Version.new('4.2.4')
       unsupported_reason_add(:conversion_host, 'RHV API version does not support conversion_host')
     end
   end
