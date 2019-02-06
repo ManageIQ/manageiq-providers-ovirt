1b3c50986c0b2963316bafe610f1b5c130054b8e
Adam Grare <agrare@redhat.com>
2019-02-06 14:29:16 -0500

    Sort records by ID when doing full table compares

--- spec/models/manageiq/providers/redhat/infra_manager/refresh/ovirt_refresher_spec_common.rb
+++ spec/models/manageiq/providers/redhat/infra_manager/refresh/ovirt_refresher_spec_common.rb
@@ -7,7 +7,7 @@ module OvirtRefresherSpecCommon
     models.each do |model|
       inventory[model.name] = model.all.collect do |rec|
         rec.attributes.except(*skip_attributes)
-      end
+      end.sort { |rec| rec["id"] }
     end
     inventory
   end
