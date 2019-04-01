fbc2a4b7aff1a092736066899076d25099154c64
Satoe Imaishi <simaishi@redhat.com>
2019-04-01 11:27:41 -0400

    Revert "Merge pull request #315 from djberg96/supports_conversion_host"
    This reverts commit 618cefad991ad7b3219a35a55bd0081fdef862e2.

--- spec/models/manageiq/providers/redhat/infra_manager/host_spec.rb
+++ spec/models/manageiq/providers/redhat/infra_manager/host_spec.rb
@@ -3,26 +3,12 @@ describe ManageIQ::Providers::Redhat::InfraManager::Host do
   describe '#quickStats' do
     let(:ems) { FactoryGirl.create(:ems_redhat_with_authentication) }
     subject { FactoryGirl.create(:host_redhat, :ems_id => ems.id) }
-
     before(:each) do
       allow_any_instance_of(ManageIQ::Providers::Redhat::InfraManager)
         .to receive(:supported_api_versions).and_return([4])
     end
-
     it '.supports_quick_stats?' do
       expect(subject.supports_quick_stats?).to be true
-    end
-
-    it '.supports_conversion_host?' do
-      allow(subject.ext_management_system).to receive(:api_version).and_return('4.2.4')
-      expect(subject.supports_conversion_host?).to be true
-    end
-
-    it 'does not support_conversion_host? if the minimum api_version is not met' do
-      message = 'RHV API version does not support conversion_host'
-      allow(subject.ext_management_system).to receive(:api_version).and_return('4.2.3')
-      expect(subject.supports_conversion_host?).to be false
-      expect(subject.unsupported_reason(:conversion_host)).to eql(message)
     end
 
     it 'calls list on StatisticsService' do
