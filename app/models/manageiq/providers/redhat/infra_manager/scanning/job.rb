5b1fb5138f236e065ed20fb4b9e70964cf22c81e
Oleg Barenboim <chessbyte@gmail.com>
2019-12-11 13:56:21 -0500

    leveraged memoized `vm` method

--- app/models/manageiq/providers/redhat/infra_manager/scanning/job.rb
+++ app/models/manageiq/providers/redhat/infra_manager/scanning/job.rb
@@ -1,11 +1,11 @@
 class ManageIQ::Providers::Redhat::InfraManager::Scanning::Job < VmScan
-  def config_ems_list(vm)
+  def config_ems_list
     super.tap do |ems_list|
       ems_list['connect'] = false
     end
   end
 
-  def create_scan_args(vm)
+  def create_scan_args
     super.tap do |scan_args|
       scan_args['permissions'] = {'group' => 36}
     end
