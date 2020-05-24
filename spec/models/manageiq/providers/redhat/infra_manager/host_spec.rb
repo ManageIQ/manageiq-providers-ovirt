cb7799cd235170a16bb4541901c5500e28d72bde
Fabien Dupont <fdupont@redhat.com>
2020-05-24 09:06:18 -0400

    Delete Migration feature - oVirt

--- spec/models/manageiq/providers/redhat/infra_manager/host_spec.rb
+++ spec/models/manageiq/providers/redhat/infra_manager/host_spec.rb
@@ -9,18 +9,6 @@ describe ManageIQ::Providers::Redhat::InfraManager::Host do
       expect(subject.supports_quick_stats?).to be true
     end
 
-    it '.supports_conversion_host?' do
-      allow(subject.ext_management_system).to receive(:api_version).and_return('4.2.4')
-      expect(subject.supports_conversion_host?).to be true
-    end
-
-    it 'does not support_conversion_host? if the minimum api_version is not met' do
-      message = 'RHV API version does not support conversion_host'
-      allow(subject.ext_management_system).to receive(:api_version).and_return('4.2.3')
-      expect(subject.supports_conversion_host?).to be false
-      expect(subject.unsupported_reason(:conversion_host)).to eql(message)
-    end
-
     it 'calls list on StatisticsService' do
       allow(subject.ext_management_system).to receive(:api_version).and_return('4.3.6')
       expect_any_instance_of(OvirtSDK4::StatisticsService).to receive(:list)
