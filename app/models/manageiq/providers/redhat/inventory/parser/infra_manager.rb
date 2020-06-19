7d4d7312a264da567011c4e2570a885b2ebcae21
Daniel Berger <djberg96@gmail.com>
2020-06-19 09:51:39 -0400

    Don't assume cpu_tune property is set.

--- app/models/manageiq/providers/redhat/inventory/parser/infra_manager.rb
+++ app/models/manageiq/providers/redhat/inventory/parser/infra_manager.rb
@@ -374,7 +374,7 @@ class ManageIQ::Providers::Redhat::Inventory::Parser::InfraManager < ManageIQ::P
       parent_folder = persister.ems_folders.lazy_find("#{datacenter_id}_vm")
       resource_pool = persister.resource_pools.lazy_find("#{vm.cluster.id}_respool") unless template
       host          = persister.hosts.lazy_find(host_ems_ref) if host_ems_ref.present?
-      cpu_affinity  = vm.cpu.cpu_tune&.vcpu_pins&.map(&:vcpu)&.join(',')
+      cpu_affinity  = vm.cpu&.cpu_tune&.vcpu_pins&.map(&:vcpu)&.join(',')
 
       storages, disks = storages(vm)
 
