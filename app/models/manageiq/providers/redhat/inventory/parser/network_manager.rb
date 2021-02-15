a63b89f13bf031abc739d573c0accb87e389b70b
Aliaksei Hrechushkin <ahrechushkin@ibagoup.eu>
2021-02-15 15:33:07 +0300

    Implement cloud tenant mapping for ovirt provider

--- app/models/manageiq/providers/redhat/inventory/parser/network_manager.rb
+++ app/models/manageiq/providers/redhat/inventory/parser/network_manager.rb
@@ -2,6 +2,10 @@ class ManageIQ::Providers::Redhat::Inventory::Parser::NetworkManager < ManageIQ:
   def parse
     super
     cloud_tenants
+    cloud_networks
+    cloud_subnets
+    network_routers
+    security_groups
   end
 
   def cloud_tenants
@@ -15,6 +19,34 @@ class ManageIQ::Providers::Redhat::Inventory::Parser::NetworkManager < ManageIQ:
     end
   end
 
+  def cloud_networks
+    collector.cloud_networks.each do |n|
+      tenant = cloud_tenant_mapper(n["name"])
+      network.cloud_tenant = tenant || persister.cloud_tenants.lazy_find(n["tenant_id"])
+    end
+  end
+
+  def cloud_subnets
+    collector.cloud_subnets.each do |s|
+      tenant = cloud_tenant_mapper(s.name)
+      subnet.cloud_tenant = tenant || persister.cloud_tenants.lazy_find(s.tenant_id)
+    end
+  end
+
+  def network_routers
+    collector.network_routers.each do |nr|
+      tenant = cloud_tenant_mapper(nr.name)
+      network_router.cloud_tenant = tenant || persister.cloud_tenants.lazy_find(nr.tenant_id)
+    end
+  end
+
+  def security_groups
+    collector.security_groups.each do |s|
+      tenant = cloud_tenant_mapper(s.name)
+      security_group.cloud_tenant = tenant || persister.cloud_tenants.lazy_find(s.tenant_id)
+    end
+  end
+
   def find_device_object(network_port)
     super || find_ovirt_device_object(network_port)
   end
@@ -25,5 +57,10 @@ class ManageIQ::Providers::Redhat::Inventory::Parser::NetworkManager < ManageIQ:
     nil unless network_port.device_owner&.downcase == 'ovirt'
 
     persister.guest_devices.lazy_find({:uid_ems => network_port.device_id}, {:ref => :by_uid_ems})
+  end
+
+  def cloud_tenant_mapper(name)
+    name_parts = name.split(::Settings.ems_ovirt.cloud_tenant_mapper.separator)
+    CloudTenant.find_by(:name => name_parts[::Settings.ems_ovirt.cloud_tenant_mapper.account])
   end
 end
