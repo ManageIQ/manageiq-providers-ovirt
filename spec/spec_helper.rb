27244e788019938c81ed60f39f0744e9778eddba
Keenan Brock <keenan@thebrocks.net>
2021-05-11 09:46:30 -0400

    Merge pull request #558 from agrare/pluggable_provider_secrets
    Use pluggable provider secrets
    
    (cherry picked from commit 2d05c7ce0844d560293052f4bad9748c8bb9ace0)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,9 @@ require "manageiq-providers-ovirt"
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::Ovirt::Engine.root, 'spec/vcr_cassettes')
+
+  secrets = Rails.application.secrets
+  secrets.ovirt.each_key do |secret|
+    config.define_cassette_placeholder(secrets.ovirt_defaults[secret]) { secrets.ovirt[secret] }
+  end
 end
