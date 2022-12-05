fb76c531a4a65ed40e54471d221d5978fd8c0653
nasark <nasarak_786@hotmail.com>
2022-12-05 10:17:52 -0500

    add supports iso_datastore feature

--- app/models/manageiq/providers/ovirt/infra_manager/iso_datastore.rb
+++ app/models/manageiq/providers/ovirt/infra_manager/iso_datastore.rb
@@ -1,4 +1,6 @@
 class ManageIQ::Providers::Ovirt::InfraManager::IsoDatastore < ManageIQ::Providers::Ovirt::InfraManager::Storage
+  supports :iso_datastore
+
   belongs_to :ext_management_system, :foreign_key => :ems_id
 
   def self.display_name(number = 1)
