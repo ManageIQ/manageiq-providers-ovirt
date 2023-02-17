a32d7be888f8dd394d388e612066a0d3c236add2
Adam Grare <adam@grare.com>
2023-02-17 11:02:23 -0500

    Remove the OVN NetworkManager Event Catcher

--- app/models/manageiq/providers/ovirt/network_manager/event_catcher.rb
+++
@@ -1,9 +0,0 @@
-class ManageIQ::Providers::Ovirt::NetworkManager::EventCatcher < ::MiqEventCatcher
-  include ManageIQ::Providers::Openstack::EventCatcherMixin
-
-  require_nested :Runner
-
-  def self.settings_name
-    :event_catcher_ovirt_network
-  end
-end
