cfed78ffc58279125573248c5527ff5a2febb0c4
Keenan Brock <keenan@thebrocks.net>
2023-03-07 12:32:56 -0500

    Use default host_verify_credentials
    The default verify_credentials used to be ws, but is now ssh.
    This code wants the new default behavior.
    
    Deleting this copy/paste code

--- app/models/manageiq/providers/ovirt/infra_manager/host.rb
+++ app/models/manageiq/providers/ovirt/infra_manager/host.rb
@@ -3,20 +3,6 @@ class ManageIQ::Providers::Ovirt::InfraManager::Host < ::Host
     ManageIQ::Providers::Ovirt::InfraManager::OvirtServices::V4.new(:ems => ext_management_system).get_host_proxy(self, connection)
   end
 
-  def verify_credentials(auth_type = nil, options = {})
-    raise MiqException::MiqHostError, "No credentials defined" if missing_credentials?(auth_type)
-    if auth_type.to_s != 'ipmi' && os_image_name !~ /linux_*/
-      raise MiqException::MiqHostError, "Logon to platform [#{os_image_name}] not supported"
-    end
-    case auth_type.to_s
-    when 'ipmi' then verify_credentials_with_ipmi(auth_type)
-    else
-      verify_credentials_with_ssh(auth_type, options)
-    end
-
-    true
-  end
-
   supports :capture
   supports :quick_stats do
     unless ext_management_system.supports?(:quick_stats)
