ffa524f1da4fdb8963bce8d75538c0b01342c388
Adam Grare <adam@grare.com>
2023-03-29 17:47:26 -0400

    Stop Ovirt OVN Event Catcher from starting

--- app/models/manageiq/providers/ovirt/network_manager/event_catcher.rb
+++ app/models/manageiq/providers/ovirt/network_manager/event_catcher.rb
@@ -3,6 +3,10 @@ class ManageIQ::Providers::Ovirt::NetworkManager::EventCatcher < ::MiqEventCatch
 
   require_nested :Runner
 
+  def self.all_valid_ems_in_zone
+    []
+  end
+
   def self.settings_name
     :event_catcher_ovirt_network
   end
